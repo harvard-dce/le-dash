17d950930ee80ceaba70f4bbc358f557989381d8
Phoebe Miller <pmiller@dce.harvard.edu>
2017-05-15 13:39:40 -0400

    Displays courses from current term for nav
    TODO: select other terms, scope menu bars

--- le_dash/banner.py
+++ le_dash/banner.py
@@ -1,20 +1,20 @@
-import six
 import hashlib
+import logging
+
 import requests
+import six
 from django.conf import settings
 from django.core.cache import cache
-import logging
 
 if six.PY2:
     from urlparse import urljoin
 else:
     from urllib.parse import urljoin
 
-
 logger = logging.getLogger(__name__)
 
 
-def banner_req(url, params):
+def banner_req(url, params={}):
     #  md5 is cheaper than urlunparse/urlencode params
     get_url = hashlib.md5("{0}".format(params))
     params['fmt'] = 'json'
@@ -52,3 +52,29 @@ def get_student_list(series_id, registered=True):
         students = [x for x in students if x['status'] == 'Registered']
 
     return students
+
+
+def get_current_term():
+    url = urljoin(settings.BANNER_BASE_URL, "__get_current_termcode.php")
+    term = banner_req(url)
+    if isinstance(term, dict):
+        return term["term_code"]
+    return None
+
+
+def get_current_course_list(termcode=None):
+    if not termcode:
+        termcode = get_current_term()
+    url = urljoin(settings.BANNER_BASE_URL, "__get_courses_by_term.php")
+    params = {
+        'term': termcode,
+    }
+    courses_data = banner_req(url, params)
+    if courses_data:
+        courses = courses_data['courses'].get('course', [])
+        if isinstance(courses, dict):
+            courses = [courses]
+        for x in courses:
+            x["fullcrn"] = termcode + x["crn"]
+        return courses
+    return []
--- le_dash/views.py
+++ le_dash/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+
+from banner import get_current_course_list, get_current_term
 from . import __version__
 
 
 def home(request):
-    return render(request, 'home.html')
+    term = get_current_term()
+    courses = get_current_course_list(term)
+    context = {
+        'courses': courses,
+        'term': term
+    }
+    return render(request, 'home.html', context)
 
 
 def about(request):
